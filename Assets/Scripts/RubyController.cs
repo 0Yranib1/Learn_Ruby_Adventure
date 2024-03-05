da3a9ca83c4926db4500c0b708bafefd278d6859
William_Yranib <a729121423@gmail.com>
2024-03-05 23:11:15 +0800

    伤害区域 敌人

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    
+    bool isInvincible;
+    float invincibleTimer;
+    
     int currentHealth;
     
     public float speed = 3.0f;
@@ -19,7 +24,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         
-        currentHealth = maxHealth-1;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -27,6 +32,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
     
     void FixedUpdate()
@@ -40,6 +52,15 @@ public class RubyController : MonoBehaviour
     
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
